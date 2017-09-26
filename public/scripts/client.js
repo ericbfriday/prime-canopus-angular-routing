6ed144edc504d22dd3e185c00c1f90cd8eb11b42
Millie Walsh <millie@primeacademy.io>
2017-09-26 10:20:18 -0500

    remove hashes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,14 +1,18 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config(function($routeProvider) {
+myApp.config(function($routeProvider, $locationProvider) {
     $routeProvider.when('/', {
         templateUrl: 'views/home.html',
-        controller: 'HomeController as hc'
+        controller: 'HomeController',
+        controllerAs: 'hc'
     }).when('/blue', {
         templateUrl: 'views/blue.html',
         controller: 'BlueController as bc'
     }).when('/red', {
         templateUrl: 'views/red.html',
         controller: 'RedController as rc'
-    }).otherwise({ redirectTo: '/'});
+    }).otherwise('/');
+    
+    // this is only for removing #!
+    $locationProvider.html5Mode(true);
   });
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.use(express.static('public'));
+
+// this MUST be the last route
+// only for removing the #! and allowing the refresh to function
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(3003, function() {
     console.log('on 3003');
